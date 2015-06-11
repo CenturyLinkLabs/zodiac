34fb8fc1e9de63930f10a56bd255213c2463b680
Brian DeHamer <brian@dehamer.com>
2015-06-11 16:44:02 +0000

    proxy refactoring
    Signed-off-by: Alex Welch <me@alexwelch.com>

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -23,10 +23,7 @@ type HTTPProxy struct {
 }
 
 func NewHTTPProxy(listenAt string) *HTTPProxy {
-	return &HTTPProxy{
-		containerRequests: make([]cluster.ContainerRequest, 0),
-	}
-
+	return &HTTPProxy{}
 }
 
 func (p *HTTPProxy) Serve(endpoint cluster.Endpoint) error {
@@ -35,9 +32,6 @@ func (p *HTTPProxy) Serve(endpoint cluster.Endpoint) error {
 	r.Path("/v1.15/containers/{id}/json").Methods("GET").HandlerFunc(p.inspect)
 	r.Path("/v1.15/containers/{id}/start").Methods("POST").HandlerFunc(p.start)
 	r.Path("/v1.15/containers/json").Methods("GET").HandlerFunc(p.listAll)
-	r.Path("/{rest:.*}").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		catchAll(endpoint, w, r)
-	})
 
 	// TODO: port should be configurable
 	laddr, _ := net.ResolveTCPAddr("tcp", "localhost:3000")
@@ -46,42 +40,13 @@ func (p *HTTPProxy) Serve(endpoint cluster.Endpoint) error {
 	return http.Serve(listener, r)
 }
 
-func catchAll(endpoint cluster.Endpoint, w http.ResponseWriter, r *http.Request) {
-	log.Infof("unhandled request to %s", r.URL)
-
-	log.Debugf("Logging request HEADERs")
-	for k, v := range r.Header {
-		log.Debugf(k, v)
-	}
-
-	r.URL.Host = endpoint.Host()
-	// TODO: we should give the scheme some thought
-	r.URL.Scheme = "http"
-	req, err := http.NewRequest(r.Method, r.URL.String(), r.Body)
-	req.Header.Set("content-type", "application/json")
-	c := http.Client{}
-	resp, err := c.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	resp.Header.Set("content-type", "application/json")
-
-	log.Debugf("Logging RESPONSE HEADERs")
-	for k, v := range resp.Header {
-		log.Debugf(k, v)
-	}
-
-	// TODO, is there a better way?
-	respBody, _ := ioutil.ReadAll(resp.Body)
-	fmt.Fprintf(w, string(respBody))
-}
-
 func (p *HTTPProxy) Stop() error {
 	p.listener.Close()
 	return nil
 }
 
 func (p *HTTPProxy) DrainRequests() []cluster.ContainerRequest {
+	// TODO maybe drain errors through this method as well
 	// TODO implement, and don't forget that this is supposed to 'drain', as in
 	// remove the saved requests that this instance has.
 	return p.containerRequests
@@ -89,14 +54,12 @@ func (p *HTTPProxy) DrainRequests() []cluster.ContainerRequest {
 
 func (p *HTTPProxy) create(w http.ResponseWriter, r *http.Request) {
 	log.Infof("CREATE request to %s", r.URL)
-	//log.Infof("CREATE form %s", r.Form)
 
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		//TODO: return errors to compose
 	}
 
-	//TODO: not all requests have a name if the service is being restarted
 	req := cluster.ContainerRequest{
 		Name:          r.URL.Query()["name"][0],
 		CreateOptions: body,
@@ -104,22 +67,16 @@ func (p *HTTPProxy) create(w http.ResponseWriter, r *http.Request) {
 
 	p.containerRequests = append(p.containerRequests, req)
 
-	fmt.Fprintf(w, `{ "Id":"foo", "Warnings":[] }`)
+	fmt.Fprintf(w, `{ "Id":"doesnt_matter", "Warnings":[] }`)
 }
 
 func (p *HTTPProxy) inspect(w http.ResponseWriter, r *http.Request) {
 	log.Infof("INSPECT request to %s", r.URL)
-	fmt.Fprintf(w, `{"Id": "foo", "Name": "/hangry_welch"}`)
+	fmt.Fprintf(w, `{"Id": "doesnt_matter", "Name": "doesnt_matter"}`)
 }
 
 func (p *HTTPProxy) start(w http.ResponseWriter, r *http.Request) {
 	log.Infof("START request to %s", r.URL)
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		//TODO: return errors to compose
-	}
-
-	log.Infof(string(body))
 	w.WriteHeader(204)
 }
 
